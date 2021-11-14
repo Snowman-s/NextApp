6d358615d7a1154395f8c0032e75ae79050002a9
Snowman-s <snowman.snowsnow@gmail.com>
2021-11-14 16:05:56 +0900

    共通のヘッダーを追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,27 @@
-import '/styles/globals.css'
-import { AppProps } from 'next/app'
-import React from 'react'
+import "/styles/globals.css";
+import { AppProps } from "next/app";
+import React from "react";
+import { AppBar, Toolbar, Typography } from "@material-ui/core";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   React.useEffect(() => {
     // Remove the server-side injected CSS.
-    const jssStyles = document.querySelector('#jss-server-side')
-    jssStyles?.parentElement?.removeChild(jssStyles)
-  }, [])
+    const jssStyles = document.querySelector("#jss-server-side");
+    jssStyles?.parentElement?.removeChild(jssStyles);
+  }, []);
 
-  return <Component {...pageProps} />
+  return (
+    <>
+      <header>
+        <AppBar position="relative">
+          <Toolbar>
+            <Typography variant="h6" color="inherit" noWrap>
+              {"SnowEsamoscのページ"}
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </header>
+      <Component {...pageProps} />{" "}
+    </>
+  );
 }

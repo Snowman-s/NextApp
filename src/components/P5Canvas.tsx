5ced0247b317c99a1903f39f0d6a57b9b675b8c5
Snowman-s <snowman.snowsnow@gmail.com>
2023-08-29 10:49:11 +0900

    P5Canvasをまた修正

--- src/components/P5Canvas.tsx
+++ src/components/P5Canvas.tsx
@@ -2,53 +2,8 @@ import { CustomP5, CustomP5Props } from "src/others/CustomP5";
 import React, { useEffect, useRef } from "react";
 
 import p5 from "p5";
+import dynamic from "next/dynamic";
 
-export default function P5Canvas(props: CustomP5Props) {
-  const parentRef = useRef();
-  const p5Inst = useRef<CustomP5>(null);
-
-  useEffect(() => {
-    killP5(p5Inst.current);
-    p5Inst.current = new p5(props.sketch, parentRef.current);
-  }, [props.sketch]);
-
-  useEffect(() => {
-    if (p5Inst.current == null) return;
-
-    if (props.saveRequire) {
-      p5Inst.current.onSave?.();
-      props.onSaveEnd?.();
-    }
-    if (props.restartRequire) {
-      p5Inst.current.onRestart?.();
-      props.onRestartEnd?.();
-    }
-    p5Inst.current.onPropsUpdate?.(props);
-  }, [props, p5Inst]);
-
-  useEffect(
-    () => () => {
-      killP5(p5Inst.current);
-      p5Inst.current = null;
-    },
-    []
-  );
-
-  return (
-    <div style={{ position: "relative" }}>
-      <div ref={parentRef} style={{ position: "absolute", zIndex: -1 }}></div>
-      {props.children}
-    </div>
-  );
-}
-
-async function killP5(p5: CustomP5 | null) {
-  if (p5 == null) return;
-
-  while (!(p5 as unknown as { _setupDone: boolean })._setupDone) {
-    await new Promise((resolve) => setTimeout(resolve, 10));
-  }
-
-  console.error("Kill", (p5 as unknown as { _setupDone: boolean })._setupDone);
-  p5.remove();
-}
+export default dynamic(() => import("./P5CanvasImpl"), {
+  ssr: false,
+});
--- src/pages/p5sketches/celebrate.tsx
+++ src/pages/p5sketches/celebrate.tsx
@@ -1,58 +1,87 @@
-import { Grid, Input, Slider, Switch, TextField, Typography} from "@material-ui/core"
-import Head from "next/head"
-import { useState } from "react"
-import { CanvasOperationPanel } from "src/components/CanvasOperationPanel"
-import P5Canvas from "src/components/P5Canvas"
-import celebrate from "src/others/sketches/celebrate/celebrate"
+import {
+  Grid,
+  Input,
+  Slider,
+  Switch,
+  TextField,
+  Typography,
+} from "@material-ui/core";
+import Head from "next/head";
+import { useState } from "react";
+import { CanvasOperationPanel } from "src/components/CanvasOperationPanel";
+import P5Canvas from "src/components/P5Canvas";
+import celebrate from "src/others/sketches/celebrate/celebrate";
 
 export default function Home() {
   const [restart, setRestart] = useState(false);
   const [save, setSave] = useState(false);
   const [happiness, setHappiness] = useState(50);
-  const [celebrateString, setCelebrateString] = useState("なにかしあわせなこと");
+  const [celebrateString, setCelebrateString] =
+    useState("なにかしあわせなこと");
 
   return (
     <div>
       <Head>
         <title>せれぶれいと </title>
         <meta name="description" content="記入した文を凄まじくお祝いします" />
-        <link rel="icon" href={process.env.NEXT_PUBLIC_ASSET_PREFIX + "/favicon.ico"} />
+        <link
+          rel="icon"
+          href={process.env.NEXT_PUBLIC_ASSET_PREFIX + "/favicon.ico"}
+        />
       </Head>
-      <P5Canvas 
-        sketch={celebrate} 
-        restartRequire={restart} 
-        onRestartEnd={()=>{setRestart(false)}}
-        saveRequire={save} 
-        onSaveEnd={()=>{setSave(false)}}
-        
-        happiness = {happiness}
-        celebrateString = {celebrateString}> 
-        <CanvasOperationPanel title="せれぶれいと" onRequireRestart={()=>{setRestart(true)}} onRequireSave={()=>{setSave(true)}} slideOutOnRestart={true}>
+      <P5Canvas
+        sketch={celebrate}
+        restartRequire={restart}
+        onRestartEnd={() => {
+          setRestart(false);
+        }}
+        saveRequire={save}
+        onSaveEnd={() => {
+          setSave(false);
+        }}
+        happiness={happiness}
+        celebrateString={celebrateString}
+      >
+        <CanvasOperationPanel
+          title="せれぶれいと"
+          onRequireRestart={() => {
+            setRestart(true);
+          }}
+          onRequireSave={() => {
+            setSave(true);
+          }}
+          slideOutOnRestart={true}
+        >
           <Grid container direction="column">
-          <Grid item>
-            <Grid container direction="row" spacing={2}>
-              <Grid item xs>
-                <Typography gutterBottom>
-                  祝いたいこと
-                </Typography>
-                <Input onChange = {(event) => {setCelebrateString(event.target.value)}}/>
+            <Grid item>
+              <Grid container direction="row" spacing={2}>
+                <Grid item xs>
+                  <Typography gutterBottom>祝いたいこと</Typography>
+                  <Input
+                    onChange={(event) => {
+                      setCelebrateString(event.target.value);
+                    }}
+                  />
+                </Grid>
               </Grid>
             </Grid>
-          </Grid>
-          <Grid item>
-            <Grid container direction="row" spacing={2}>
-              <Grid item xs>
-                <Typography gutterBottom>
-                  ハピネス度 (負荷注意！)
-                </Typography>
-                <Slider max={100} defaultValue={50} onChange={(event:object,value:number)=>{setHappiness(value);}}/>
+            <Grid item>
+              <Grid container direction="row" spacing={2}>
+                <Grid item xs>
+                  <Typography gutterBottom>ハピネス度 (負荷注意！)</Typography>
+                  <Slider
+                    max={100}
+                    defaultValue={50}
+                    onChange={(event: object, value: number) => {
+                      setHappiness(value);
+                    }}
+                  />
+                </Grid>
               </Grid>
             </Grid>
-          </Grid>
           </Grid>
         </CanvasOperationPanel>
       </P5Canvas>
     </div>
-  )
+  );
 }
-  

33def9491fbfbf58f287f7861d6fb187e156ada8
Snowman-s <snowman.snowsnow@gmail.com>
2025-06-29 14:20:28 +0900

    circleci

--- next.config.js
+++ next.config.js
@@ -8,6 +8,10 @@ const nextConfig = {
   basePath: production ? "/NextApp" : "",
   assetPrefix: production ? "/NextApp" : "/",
   trailingSlash: true,
+  output: "export",
+  images: {
+    unoptimized: true
+  }
 };
 
 module.exports = nextConfig;
